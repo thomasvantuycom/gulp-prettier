9b7936d466a7ed1e2b042fa566fd413eadc05aef
Thomas Vantuycom <thomasvantuycom@protonmail.com>
2018-03-19 23:01:16 +0100

    refactor: get package name from package.json

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const PluginError = require('plugin-error');
 const prettier = require('prettier');
 const applySourceMap = require('vinyl-sourcemaps-apply');
 
+const pkg = require('./package.json');
+
 module.exports = function(options) {
   function transform(file, encoding, callback) {
     if (file.isNull()) return callback(null, file);
     if (file.isStream())
-      return callback(
-        new PluginError('gulp-prettier', 'Streaming not supported')
-      );
+      return callback(new PluginError(pkg.name, 'Streaming not supported'));
 
     let data;
     const str = file.contents.toString('utf8');
@@ -18,7 +18,7 @@ module.exports = function(options) {
     try {
       data = prettier.format(str, options);
     } catch (err) {
-      return callback(new PluginError('gulp-prettier', err));
+      return callback(new PluginError(pkg.name, err));
     }
 
     if (data && data.v3SourceMap && file.sourceMap) {
