af9c483b05f733562d683ac381ff190baac3607a
Thomas Vantuycom <thomasvantuycom@protonmail.com>
2018-03-19 22:32:45 +0100

    refactor: simplify options construct

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 const through = require('through2');
 const PluginError = require('plugin-error');
 const prettier = require('prettier');
-const merge = require('merge');
 const applySourceMap = require('vinyl-sourcemaps-apply');
 
-module.exports = function(opt) {
+module.exports = function(options) {
   function transform(file, encoding, callback) {
     if (file.isNull()) return callback(null, file);
     if (file.isStream())
@@ -15,28 +14,6 @@ module.exports = function(opt) {
 
     let data;
     const str = file.contents.toString('utf8');
-
-    const options = merge(
-      {
-        // Fit code within this line limit
-        printWidth: 80,
-        // Number of spaces it should use per tab
-        tabWidth: 2,
-        // Use tabs instead of spaces
-        useTabs: false,
-        // Remove semicolons
-        semi: true,
-        // If true, will use single instead of double quotes
-        singleQuote: false,
-        // Controls the printing of trailing commas wherever possible
-        trailingComma: 'none',
-        // Controls the printing of spaces inside array and objects
-        bracketSpacing: true,
-        // Put JSX angle brackets on a new line rather than the last line of attributes
-        jsxBracketSameLine: false
-      },
-      opt
-    );
 
     try {
       data = prettier.format(str, options);
