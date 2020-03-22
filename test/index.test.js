3bc5aac508700e92d81ea21a927de9a569023770
Thomas Vantuycom <thomasvantuycom@protonmail.com>
2020-03-22 12:04:18 +0100

    test: migrate to ava

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const test = require('ava');
 const path = require('path');
 const { Readable } = require('stream');
 const PluginError = require('plugin-error');
@@ -7,302 +7,292 @@ const Vinyl = require('vinyl');
 const pkg = require('../package.json');
 const plugin = require('..');
 
-describe('gulp-prettier', () => {
-  it('should pass through file when file isNull()', (done) => {
-    const stream = plugin();
+test.cb('passes through file when file isNull()', (t) => {
+  const stream = plugin();
 
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'empty.js'),
-      contents: null,
-    });
-
-    stream.on('data', (file) => {
-      assert.strictEqual(file.isNull(), true);
-      assert.strictEqual(file.relative, 'empty.js');
-      done();
-    });
-
-    stream.write(file);
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'empty.js'),
+    contents: null,
   });
 
-  it('should emit an error when file isStream()', (done) => {
-    const stream = plugin();
-
-    const input = new Readable();
-    input.push('var foor = bar');
-    input.push(null);
-
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'stream.js'),
-      contents: input,
-    });
-
-    stream.on('error', (err) => {
-      assert.ok(err instanceof PluginError);
-      assert.strictEqual(err.plugin, pkg.name);
-      assert.strictEqual(err.message, 'Streaming not supported');
-      done();
-    });
-
-    stream.write(file);
+  stream.on('data', (file) => {
+    t.true(file.isNull());
+    t.is(file.relative, 'empty.js');
+    t.end();
   });
 
-  it('should format a file with Prettier', (done) => {
-    const stream = plugin();
-
-    const input = "var foo = 'bar'";
-
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'default.js'),
-      contents: Buffer.from(input),
-    });
-
-    stream.once('data', (file) => {
-      assert.ok(file.isBuffer());
-      assert.strictEqual(file.relative, 'default.js');
-      assert.strictEqual(file.contents.toString('utf8'), 'var foo = "bar";\n');
-      done();
-    });
-
-    stream.write(file);
-  });
-
-  it('should pass options to Prettier', (done) => {
-    const options = { singleQuote: true, semi: false };
-
-    const stream = plugin(options);
-
-    const input = "var foo = 'bar'";
-
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'options.js'),
-      contents: Buffer.from(input),
-    });
-
-    stream.once('data', (file) => {
-      assert.ok(file.isBuffer());
-      assert.strictEqual(file.relative, 'options.js');
-      assert.strictEqual(file.contents.toString('utf8'), "var foo = 'bar'\n");
-      done();
-    });
-
-    stream.write(file);
-  });
-
-  it('should emit an error when Prettier errors', (done) => {
-    const stream = plugin();
-
-    const input = 'var foo = \'bar"';
-
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'error.js'),
-      contents: Buffer.from(input),
-    });
-
-    stream.on('error', (err) => {
-      assert.ok(err instanceof PluginError);
-      assert.strictEqual(err.plugin, pkg.name);
-      assert.strictEqual(err.name, 'SyntaxError');
-      assert.strictEqual(
-        err.fileName,
-        path.join(process.cwd(), 'src', 'error.js')
-      );
-      done();
-    });
-
-    stream.write(file);
-  });
-
-  it('should adhere to .prettierrc', (done) => {
-    const stream = plugin();
-
-    const input = "var foo = 'bar'";
-
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(__dirname, 'fixtures'),
-      path: path.join(__dirname, 'fixtures', 'config.js'),
-      contents: Buffer.from(input),
-    });
-
-    stream.once('data', (file) => {
-      assert.ok(file.isBuffer());
-      assert.strictEqual(file.relative, 'config.js');
-      assert.strictEqual(file.contents.toString('utf8'), "var foo = 'bar'\n");
-      done();
-    });
-
-    stream.write(file);
-  });
-
-  it('should override .prettierrc with plugin options', (done) => {
-    const options = { semi: true };
-
-    const stream = plugin(options);
-
-    const input = "var foo = 'bar'";
-
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(__dirname, 'fixtures'),
-      path: path.join(__dirname, 'fixtures', 'config.js'),
-      contents: Buffer.from(input),
-    });
-
-    stream.once('data', (file) => {
-      assert.ok(file.isBuffer());
-      assert.strictEqual(file.relative, 'config.js');
-      assert.strictEqual(file.contents.toString('utf8'), "var foo = 'bar';\n");
-      done();
-    });
-
-    stream.write(file);
-  });
-
-  it('should work with non-js files', (done) => {
-    const stream = plugin();
-
-    const input = 'div{margin:1em}';
-
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'notjs.css'),
-      contents: Buffer.from(input),
-    });
-
-    stream.once('data', (file) => {
-      assert.ok(file.isBuffer());
-      assert.strictEqual(file.relative, 'notjs.css');
-      assert.strictEqual(
-        file.contents.toString('utf8'),
-        'div {\n  margin: 1em;\n}\n'
-      );
-      done();
-    });
-
-    stream.write(file);
-  });
-
-  it('should support editorconfig', (done) => {
-    const options = { editorconfig: true };
-
-    const stream = plugin(options);
-
-    const input = 'function foo() { console.log("bar"); }';
-
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(__dirname, 'fixtures'),
-      path: path.join(__dirname, 'fixtures', 'config.js'),
-      contents: Buffer.from(input),
-    });
-
-    stream.once('data', (file) => {
-      assert.ok(file.isBuffer());
-      assert.strictEqual(file.relative, 'config.js');
-      assert.strictEqual(
-        file.contents.toString('utf8'),
-        "function foo() {\n\tconsole.log('bar')\n}\n"
-      );
-      done();
-    });
-
-    stream.write(file);
-  });
+  stream.write(file);
 });
 
-describe('gulp-prettier.check', () => {
-  it('should pass through formatted files', (done) => {
-    const stream = plugin.check();
+test.cb('emits an error when file isStream()', (t) => {
+  const stream = plugin();
 
-    const input = 'var foo = "bar";\n';
+  const input = new Readable();
+  input.push('var foo = bar');
+  input.push(null);
 
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'formatted.js'),
-      contents: Buffer.from(input),
-    });
-
-    stream.once('data', (file) => {
-      assert.ok(file.isBuffer());
-      assert.strictEqual(file.relative, 'formatted.js');
-      assert.strictEqual(file.contents.toString('utf8'), input);
-      done();
-    });
-
-    stream.write(file);
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'stream.js'),
+    contents: input,
   });
 
-  it('should error on unformatted files', (done) => {
-    const stream = plugin.check();
-
-    const input = "var foo = 'bar'";
-
-    const file = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'unformatted.js'),
-      contents: Buffer.from(input),
-    });
-
-    stream.on('error', (err) => {
-      assert.ok(err instanceof PluginError);
-      assert.strictEqual(err.plugin, pkg.name);
-      assert.ok(err.message.includes('Code style issues found'));
-      done();
-    });
-
-    stream.end(file);
+  stream.on('error', (err) => {
+    t.true(err instanceof PluginError);
+    t.is(err.plugin, pkg.name);
+    t.is(err.message, 'Streaming not supported');
+    t.end();
   });
 
-  it('should list the unformatted files in the error message', (done) => {
-    const stream = plugin.check();
+  stream.write(file);
+});
 
-    const unformattedInput = "var foo = 'bar'";
-    const formattedInput = 'var foo = "bar";\n';
+test.cb('formats a file with Prettier', (t) => {
+  const stream = plugin();
 
-    const fileA = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'a.js'),
-      contents: Buffer.from(unformattedInput),
-    });
+  const input = "var foo = 'bar'";
 
-    const fileB = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'b.js'),
-      contents: Buffer.from(formattedInput),
-    });
-
-    const fileC = new Vinyl({
-      cwd: process.cwd(),
-      base: path.join(process.cwd(), 'src'),
-      path: path.join(process.cwd(), 'src', 'c.js'),
-      contents: Buffer.from(unformattedInput),
-    });
-
-    stream.on('error', (err) => {
-      assert.ok(err.message.includes('src/a.js'));
-      assert.ok(err.message.includes('src/c.js'));
-      assert.ok(!err.message.includes('src/b.js'));
-      done();
-    });
-
-    stream.write(fileA);
-    stream.write(fileB);
-    stream.end(fileC);
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'default.js'),
+    contents: Buffer.from(input),
   });
+
+  stream.once('data', (file) => {
+    t.true(file.isBuffer());
+    t.is(file.relative, 'default.js');
+    t.is(file.contents.toString('utf8'), 'var foo = "bar";\n');
+    t.end();
+  });
+
+  stream.write(file);
+});
+
+test.cb('passes options to Prettier', (t) => {
+  const options = { singleQuote: true, semi: false };
+
+  const stream = plugin(options);
+
+  const input = "var foo = 'bar'";
+
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'options.js'),
+    contents: Buffer.from(input),
+  });
+
+  stream.once('data', (file) => {
+    t.true(file.isBuffer());
+    t.is(file.relative, 'options.js');
+    t.is(file.contents.toString('utf8'), "var foo = 'bar'\n");
+    t.end();
+  });
+
+  stream.write(file);
+});
+
+test.cb('emits an error when Prettier errors', (t) => {
+  const stream = plugin();
+
+  const input = 'var foo = \'bar"';
+
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'error.js'),
+    contents: Buffer.from(input),
+  });
+
+  stream.on('error', (err) => {
+    t.true(err instanceof PluginError);
+    t.is(err.plugin, pkg.name);
+    t.is(err.name, 'SyntaxError');
+    t.is(err.fileName, path.join(process.cwd(), 'src', 'error.js'));
+    t.end();
+  });
+
+  stream.write(file);
+});
+
+test.cb('adheres to .prettierrc', (t) => {
+  const stream = plugin();
+
+  const input = "var foo = 'bar'";
+
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(__dirname, 'fixtures'),
+    path: path.join(__dirname, 'fixtures', 'config.js'),
+    contents: Buffer.from(input),
+  });
+
+  stream.once('data', (file) => {
+    t.true(file.isBuffer());
+    t.is(file.relative, 'config.js');
+    t.is(file.contents.toString('utf8'), "var foo = 'bar'\n");
+    t.end();
+  });
+
+  stream.write(file);
+});
+
+test.cb('overrides .prettierrc with plugin options', (t) => {
+  const options = { semi: true };
+
+  const stream = plugin(options);
+
+  const input = "var foo = 'bar'";
+
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(__dirname, 'fixtures'),
+    path: path.join(__dirname, 'fixtures', 'config.js'),
+    contents: Buffer.from(input),
+  });
+
+  stream.once('data', (file) => {
+    t.true(file.isBuffer());
+    t.is(file.relative, 'config.js');
+    t.is(file.contents.toString('utf8'), "var foo = 'bar';\n");
+    t.end();
+  });
+
+  stream.write(file);
+});
+
+test.cb('works with non-js files', (t) => {
+  const stream = plugin();
+
+  const input = 'div{margin:1em}';
+
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'notjs.css'),
+    contents: Buffer.from(input),
+  });
+
+  stream.once('data', (file) => {
+    t.true(file.isBuffer());
+    t.is(file.relative, 'notjs.css');
+    t.is(file.contents.toString('utf8'), 'div {\n  margin: 1em;\n}\n');
+    t.end();
+  });
+
+  stream.write(file);
+});
+
+test.cb('supports editorconfig', (t) => {
+  const options = { editorconfig: true };
+
+  const stream = plugin(options);
+
+  const input = 'function foo() { console.log("bar"); }';
+
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(__dirname, 'fixtures'),
+    path: path.join(__dirname, 'fixtures', 'config.js'),
+    contents: Buffer.from(input),
+  });
+
+  stream.once('data', (file) => {
+    t.true(file.isBuffer());
+    t.is(file.relative, 'config.js');
+    t.is(
+      file.contents.toString('utf8'),
+      "function foo() {\n\tconsole.log('bar')\n}\n"
+    );
+    t.end();
+  });
+
+  stream.write(file);
+});
+
+test.cb('check() passes through formatted files', (t) => {
+  const stream = plugin.check();
+
+  const input = 'var foo = "bar";\n';
+
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'formatted.js'),
+    contents: Buffer.from(input),
+  });
+
+  stream.once('data', (file) => {
+    t.true(file.isBuffer());
+    t.is(file.relative, 'formatted.js');
+    t.is(file.contents.toString('utf8'), input);
+    t.end();
+  });
+
+  stream.write(file);
+});
+
+test.cb('check() errors on unformatted files', (t) => {
+  const stream = plugin.check();
+
+  const input = "var foo = 'bar'";
+
+  const file = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'unformatted.js'),
+    contents: Buffer.from(input),
+  });
+
+  stream.on('error', (err) => {
+    t.true(err instanceof PluginError);
+    t.is(err.plugin, pkg.name);
+    t.true(err.message.includes('Code style issues found'));
+    t.end();
+  });
+
+  stream.end(file);
+});
+
+test.cb('check() lists the unformatted files in the error message', (t) => {
+  const stream = plugin.check();
+
+  const unformattedInput = "var foo = 'bar'";
+  const formattedInput = 'var foo = "bar";\n';
+
+  const fileA = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'a.js'),
+    contents: Buffer.from(unformattedInput),
+  });
+
+  const fileB = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'b.js'),
+    contents: Buffer.from(formattedInput),
+  });
+
+  const fileC = new Vinyl({
+    cwd: process.cwd(),
+    base: path.join(process.cwd(), 'src'),
+    path: path.join(process.cwd(), 'src', 'c.js'),
+    contents: Buffer.from(unformattedInput),
+  });
+
+  stream.on('error', (err) => {
+    t.true(err.message.includes('src/a.js'));
+    t.true(err.message.includes('src/c.js'));
+    t.true(!err.message.includes('src/b.js'));
+    t.end();
+  });
+
+  stream.write(fileA);
+  stream.write(fileB);
+  stream.end(fileC);
 });
